832ac2e70a6ae449ce21ca55e82a2f412176ba2f
Lucasvfs <lucasviniciusfs@hotmail.com>
2020-11-20 18:07:08 -0300

    Commit 1 - Iniciando o labirinto
    Neste commit iniciei algumas das variaveis e constantes que fazem parte dos elementos do labirinto e criei uma função para exibi-lo

--- main.cpp
+++ main.cpp
@@ -2,8 +2,47 @@
 
 using namespace std;
 
-int main(){
+const char ESPACO = '*';
+const char PAREDE = 'P';
+const char QUEIJO = 'Q';
+const char ENTRADA = 'E';
+const char RATO = 'R';
+const char VOLTANDO = 'V';
+
+const int LARGURA = 5;
+const int ALTURA  = 5;
+
+void exibirMapa (char mapa[LARGURA][ALTURA])
+{
+    for (int i=0; i<LARGURA; i++)
+    {
+        for (int j=0; j<ALTURA; j++)
+        {
+            cout << mapa[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+}
+
+int main (){
+
+    char mapa [LARGURA][ALTURA];
+
+    for (int i = 0; i <LARGURA; i++){
+        for (int j = 0; j < ALTURA; j++){
+            mapa[i][j] = ESPACO;
+        }
+    }
 
 
-    return 0;
+    mapa[0][0] = ENTRADA;
+    mapa[0][4] = PAREDE;
+    mapa[1][1] = PAREDE;
+    mapa[1][4] = PAREDE;
+    mapa[2][3] = PAREDE;
+    mapa[3][3] = QUEIJO;
+
+    exibirMapa (mapa);
+
 }
